26e48e1db8504a45a197a10797713b29951b888f
anuj <anuj.singh@roor.io>
2024-04-16 14:10:03 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	const BASE_URL = ' https://d2aa6qvp8uav7z.cloudfront.net/';
+	let isLoading = true;
+	const signature = '806d87f4-5728-4771-a5f8-f491007d6dce';
+	const formId = '53d7702c-742a-4d0c-85c1-3bb9167d9b52';
+	onMount(async () => {
+		try {
+			const response = await fetch(
+				`${BASE_URL}/api/account/intake/assignment/load/completed?patient_intake_form_id=${encodeURIComponent(
+					signature ?? ''
+				)}`,
+				{
+					method: 'GET'
+				}
+			);
+			if (response?.ok) {
+				const data = await response.json();
+				console.log('data', data);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+		isLoading = false;
+	});
+</script>
+
+{#if isLoading}
+	<h1 class="text-3xl font-bold underline text-center">Loading Component</h1>
+{:else}
+	<h1 class="text-3xl font-bold underline text-center">Hello world!</h1>
+{/if}
